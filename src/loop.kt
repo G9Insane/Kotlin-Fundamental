aba37c83615d03b7243c95d4ba1b8d33fa9abbfb
Galang <2202insane@gmail.com>
2018-10-09 21:19:15 +0700

    add loop example

---
+++ src/loop.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+    //FOR LOOP
+    //
+    println("For Loop")
+    for (i in 1..4){
+        println("hello")
+    }
+
+    //WHILE LOOP:
+    var i = 0
+    println("While Loop")
+    while (i<5){
+        println("Hello")
+        i++
+    }
+    i = 0
+    //DO WHILE LOOP:
+    println("Do While Loop")
+    do{
+        println("Hello")
+        i++
+    } while (i<5)
+}
